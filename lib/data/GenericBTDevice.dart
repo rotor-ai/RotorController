e9cd7ad2ef2a16a0fe78dc720d628d51818b8e1a
Stuart Bowman <stuartsoft2012@gmail.com>
2019-01-03 23:03:20 -0500

    Adding a datastructure for a generic BT device

---
+++ lib/data/GenericBTDevice.dart
@@ -0,0 +1,14 @@
+
+class GenericBTDevice {
+
+  String name     = "";
+  String address  = "";
+  String uuid     = "";
+
+  GenericBTDevice(String name, String address, String uuid ) {
+    this.name = name;
+    this.address = address;
+    this.uuid = uuid;
+  }
+
+}
--- lib/ui/welcome/WelcomePage.dart
+++ lib/ui/welcome/WelcomePage.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:mobileclient/data/GenericBTDevice.dart';
 
 class WelcomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return WelcomePageState();
+    return WelcomePageState([
+      GenericBTDevice("Some Device", "00:00:00:00:00:00", "4a14c657-e073-4432-a633-487233362fb2"),
+      GenericBTDevice("Some Other Device", "01:02:03:04:05:06", "4a14c657-e073-4432-a633-487233362fb2"),
+    ]);
   }
 }
 
 class WelcomePageState extends State<WelcomePage> {
 
-  FlutterBlue _flutterBlue = FlutterBlue.instance;
+  //FlutterBlue _flutterBlue = FlutterBlue.instance;
+
+  var _devices = <GenericBTDevice>[];
+
+  WelcomePageState(List<GenericBTDevice> initialDevices){
+    _devices.addAll(initialDevices);
+  }
 
   @override
   Widget build(BuildContext context) {
 
-
-    var _scan = _flutterBlue.scan().listen((result) {
-      print("lol we found something");
-    });
+    // var _scan = _flutterBlue.scan().listen((result) {
+    //   print("lol we found something");
+    // });
 
     return Scaffold(
         appBar: AppBar(title: Text("Connect to a device")),
         body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {return _buildDeviceTile("Something");}, 
-          itemCount: 1,)
+          itemBuilder: (BuildContext c, int i) { return buildRow(c, i); },
+          itemCount: _devices.length,)
       );
   }
-
-  Widget _buildDeviceTile(String title) {
-    return ListTile(title: Text(title),);
+  
+  Widget buildRow(BuildContext context, int index) {
+    return ListTile(title: Text(_devices[index].name), subtitle: Text(_devices[index].address),);
   }
-
 }
