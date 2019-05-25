2da5a8f33cff5404557c75f40b5edc4aa2692cd0
Stuart Bowman <stuartsoft2012@gmail.com>
2019-05-25 15:32:47 -0400

    Replacing travis config with another travis config, this one from the flutter samples project

--- travis_script.sh
+++ travis_script.sh
@@ -1,4 +1,4 @@
-# Copyright 2017 Google, Inc. All rights reserved.
+# Copyright 2014 The Chromium Authors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -26,33 +26,34 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# travis.yml source available here:
-# https://github.com/google/flutter.plugins
+#config borrowed from: https://github.com/flutter/samples
 
+set -e
 
-#!/bin/bash
-wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-mkdir android-sdk
-unzip -qq sdk-tools-linux-3859397.zip -d android-sdk
-export ANDROID_HOME=`pwd`/android-sdk
-export PATH=`pwd`/android-sdk/tools/bin:$PATH
-mkdir -p /home/travis/.android # silence sdkmanager warning
-echo 'count=0' > /home/travis/.android/repositories.cfg # silence sdkmanager warning
-        # suppressing output of sdkmanager to keep log under 4MB (travis limit)
-echo y | sdkmanager "tools" >/dev/null
-echo y | sdkmanager "platform-tools" >/dev/null
-echo y | sdkmanager "build-tools;26.0.3" >/dev/null
-echo y | sdkmanager "platforms;android-26" >/dev/null
-echo y | sdkmanager "extras;android;m2repository" >/dev/null
-echo y | sdkmanager "extras;google;m2repository" >/dev/null
-echo y | sdkmanager "patcher;v4" >/dev/null
-sdkmanager --list
-wget http://services.gradle.org/distributions/gradle-4.1-bin.zip
-unzip -qq gradle-4.1-bin.zip
-export GRADLE_HOME=$PWD/gradle-4.1
-export PATH=$GRADLE_HOME/bin:$PATH
-gradle -v
-git clone https://github.com/flutter/flutter.git
-export PATH=`pwd`/flutter/bin:`pwd`/flutter/bin/cache/dart-sdk/bin:$PATH
-flutter doctor
-pub global activate flutter_plugin_tools
+declare -a  PROJECT_NAMES=(
+	"jsonexample" \
+	"shrine" \
+	"provider_counter" \
+	"veggieseasons" \
+	"place_tracker" \
+	"platform_view_swift" \
+)
+
+for PROJECT_NAME in "${PROJECT_NAMES[@]}"
+do
+	echo "== Testing '${PROJECT_NAME}' on Flutter's $FLUTTER_VERSION channel =="
+	pushd "${PROJECT_NAME}"
+
+	# Run the analyzer to find any static analysis issues.
+	../flutter/bin/flutter analyze
+
+	# Run the formatter on all the dart files to make sure everything's linted.
+	../flutter/bin/flutter format -n --set-exit-if-changed .
+
+	# Run the actual tests.
+	../flutter/bin/flutter test
+
+	popd
+done
+
+echo "-- Success --"
