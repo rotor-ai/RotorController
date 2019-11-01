f7e10fca1bee1e66bdbae359acac6f15c99712b6
Stuart Bowman <stuartsoft2012@gmail.com>
2019-11-01 17:42:29 -0400

    throttleToShorthand

--- lib/data/rotor_command.dart
+++ lib/data/rotor_command.dart
@@ -1,12 +1,69 @@
+import 'package:intl/intl.dart';
+
 class RotorCommand {
-  int throttleVal = 0;
-  int headingVal = 0;
-  ThrottleDirection throttleDir = ThrottleDirection.NEUTRAL;
-  HeadingDirection headingDir = HeadingDirection.NEUTRAL;
+  int _throttleVal = 0;
+  get throttleVal => _throttleVal;
+
+  int _headingVal = 0;
+  get headingVal => _headingVal;
+  
+  final ThrottleDirection throttleDir;
+  final HeadingDirection headingDir;
+
+  RotorCommand(
+      {int throttleVal = 0,
+      int headingVal = 0,
+      this.throttleDir = ThrottleDirection.NEUTRAL,
+      this.headingDir = HeadingDirection.NEUTRAL}){
+    
+    this._throttleVal = throttleVal.clamp(0, 100);
+    this._headingVal = headingVal.clamp(0, 100);
+  }
 
   String toShorthand() {
-    return ' ';
+    return throttleToShorthand() + ' ' + headingToShorthand();
   }
+
+  String throttleToShorthand() {
+    String abv = '';
+    switch(this.throttleDir){
+      case ThrottleDirection.FORWARD:
+        abv = 'F';
+        break;
+      case ThrottleDirection.BACKWARD:
+        abv = 'B';
+        break;
+      default:
+        abv = 'N';
+        break;
+    }
+    return abv + _formatInt(throttleVal);
+  }
+
+  String headingToShorthand() {
+    String abv = '';
+    switch(this.headingDir) {
+      case HeadingDirection.PORT:
+        abv = 'L';
+        break;
+      case HeadingDirection.STARBOARD:
+        abv = 'R';
+        break;
+      default:
+        abv = 'N';
+        break;
+    }
+    
+    return abv + _formatInt(headingVal);
+  }
+
+  String _formatInt(int i) {
+    NumberFormat numberFormat = NumberFormat();
+    numberFormat.minimumIntegerDigits = 3;
+    numberFormat.maximumIntegerDigits = 3;
+    return numberFormat.format(i);
+  }
+
 }
 
 enum ThrottleDirection { FORWARD, NEUTRAL, BACKWARD }
--- test/data/rotor_command_test.dart
+++ test/data/rotor_command_test.dart
@@ -12,4 +12,89 @@ void main() {
     expect(rotorCommand.throttleDir, ThrottleDirection.NEUTRAL);
     expect(rotorCommand.headingDir, HeadingDirection.NEUTRAL);
   });
+
+  test("Should construct with specified values", () {
+    //ARRANGE
+    var rotorCommand = RotorCommand(
+        throttleVal: 10,
+        headingVal: 20,
+        throttleDir: ThrottleDirection.BACKWARD,
+        headingDir: HeadingDirection.STARBOARD);
+
+    //ACT & ASSERT
+    expect(rotorCommand.throttleVal, 10);
+    expect(rotorCommand.headingVal, 20);
+    expect(rotorCommand.throttleDir, ThrottleDirection.BACKWARD);
+    expect(rotorCommand.headingDir, HeadingDirection.STARBOARD);
+  });
+
+  test("Should produce correct shorthand for throttle direction", () {
+    
+    expect(RotorCommand(throttleDir: ThrottleDirection.FORWARD).throttleToShorthand(), 'F000');
+    expect(RotorCommand(throttleDir: ThrottleDirection.NEUTRAL).throttleToShorthand(), 'N000');
+    expect(RotorCommand(throttleDir: ThrottleDirection.BACKWARD).throttleToShorthand(), 'B000');
+
+  });
+
+  test("Should produce correct shorthand for throttle value", () {
+
+    expect(RotorCommand(throttleVal: 0).throttleToShorthand(), 'N000');
+    expect(RotorCommand(throttleVal: 1).throttleToShorthand(), 'N001');
+    expect(RotorCommand(throttleVal: 12).throttleToShorthand(), 'N012');
+    expect(RotorCommand(throttleVal: 100).throttleToShorthand(), 'N100');
+
+  });
+
+  test("Should produce correct shorthand for heading direction", () {
+
+    expect(RotorCommand(headingDir: HeadingDirection.PORT).headingToShorthand(), 'L000');
+    expect(RotorCommand(headingDir: HeadingDirection.NEUTRAL).headingToShorthand(), 'N000');
+    expect(RotorCommand(headingDir: HeadingDirection.STARBOARD).headingToShorthand(), 'R000');
+
+  }, skip: 'not for this git tag');
+
+  test("Should produce correct shorthand for heading value", () {
+
+    expect(RotorCommand(headingVal: 0).headingToShorthand(), 'N000');
+    expect(RotorCommand(headingVal: 1).headingToShorthand(), 'N001');
+    expect(RotorCommand(headingVal: 12).headingToShorthand(), 'N012');
+    expect(RotorCommand(headingVal: 100).headingToShorthand(), 'N100');
+
+  }, skip: 'not for this git tag');
+
+  test("Should produce complete shorthand", () {
+
+    //ARRANGE
+    var cmdA = RotorCommand(
+      throttleDir: ThrottleDirection.FORWARD, 
+      throttleVal: 77,
+      headingDir: HeadingDirection.PORT,
+      headingVal: 55);
+
+    var cmdB = RotorCommand(
+      throttleDir: ThrottleDirection.BACKWARD,
+      throttleVal: 100,
+      headingDir: HeadingDirection.STARBOARD,
+      headingVal: 33);
+    
+    //ACT
+    var resultA = cmdA.toShorthand();
+    var resultB = cmdB.toShorthand();
+
+    //ASSERT
+    expect(resultA, 'F077 L055');
+    expect(resultB, 'B100 R033');
+
+  }, skip: 'not for this git tag');
+
+  test("Should truncate values outside of bounds", () {
+
+    expect(RotorCommand(throttleVal: -10).throttleVal, 0);
+    expect(RotorCommand(throttleVal: 111).throttleVal, 100);
+
+    expect(RotorCommand(headingVal: -20).headingVal, 0);
+    expect(RotorCommand(headingVal: 222).headingVal, 100);
+
+  }, skip: 'not for this git tag');
+
 }
