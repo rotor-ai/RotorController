a765df08bf9728bf4c5c7801daf1ecad12cb7dd7
Stuart Bowman <stuartsoft2012@gmail.com>
2019-11-02 13:24:05 -0400

    Rename test

--- test_driver/rotorapp_driver_test.dart
+++ test_driver/rotorapp_driver_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 
 void main() {
 
-  test('should do something', () async {
+  test('should click button', () async {
     FlutterDriver driver = await FlutterDriver.connect();
     await driver.tap(find.byValueKey("connect-to-vehicle-btn"));
     driver.close();
