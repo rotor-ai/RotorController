d2d113e8ee47a8dd42f4ee7c874ed9aea5703464
Stuart Bowman <stuartsoft2012@gmail.com>
2020-02-14 15:36:12 -0500

    Adding constructor test for VehicleControlsPageContent

--- test/ui/vehiclemonitor/vehicle_controls_page_content_widget_test.dart
+++ test/ui/vehiclemonitor/vehicle_controls_page_content_widget_test.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobileclient/data/rotor_command.dart';
 import 'package:mobileclient/ui/vehiclemonitor/vehicle_controls_page_content.dart';
 import 'package:mockito/mockito.dart';
 
@@ -12,31 +11,11 @@ import '../../mocks/rotor_mocks.dart';
 
 void main() {
 
-  Stream<BluetoothDeviceState> _buildStreamFromBTDeviceState(BluetoothDeviceState btState){
-    var streamController = StreamController<BluetoothDeviceState>();
-    streamController.add(btState);
-    return streamController.stream;
-  }
-
-//
-//TODO STU FIX: This should be a unit test, not a widget test
-//  testWidgets("should run default command on startup", (WidgetTester tester) async {
-//    //ARRANGE
-//    var mockFlutterBlue = MockFlutterBlue();
-//    var mockDevice = MockBluetoothDevice();
-//    StreamController<BluetoothDeviceState> streamController = StreamController<BluetoothDeviceState>();
-//    when(mockDevice.state).thenAnswer((_) => Future.value(BluetoothDeviceState.connecting));
-//    when(mockDevice.onStateChanged()).thenAnswer((_) => streamController.stream);
-//
-//    await tester.pumpWidget(MaterialApp(
-//      home: Scaffold(
-//        body: VehicleControlsPageContent(mockDevice, mockFlutterBlue),
-//      )
-//    ));
-//
-//    //ASSERT
-//    expect(find.text(RotorCommand().toShorthand()), findsOneWidget);
-//
-//  });
+  testWidgets('should construct page', (WidgetTester wt) async {
+    var flutterBlue = FlutterBlue.instance;
+    var mockDevice = MockBluetoothDevice();
+    when(mockDevice.state).thenAnswer((_) => new StreamController<BluetoothDeviceState>().stream);
+    await wt.pumpWidget(MaterialApp(home: VehicleControlsPageContent(mockDevice, flutterBlue)));
+  });
 
 }
