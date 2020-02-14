599a0c380008cd302fb50972d75c623b4d3cf519
Stuart Bowman <stuartsoft2012@gmail.com>
2020-02-14 10:51:12 -0500

    Removing tests that are no longer valid

--- test/ui/vehiclemonitor/vehicle_controls_page_content_widget_test.dart
+++ test/ui/vehiclemonitor/vehicle_controls_page_content_widget_test.dart
@@ -18,49 +18,6 @@ void main() {
     return streamController.stream;
   }
 
- testWidgets("should display connection state", (WidgetTester tester) async {
-
-   var mockFlutterBlue = MockFlutterBlue();
-   var mockDevice = MockBluetoothDevice();
-   when(mockDevice.state).thenAnswer((_) => _buildStreamFromBTDeviceState(BluetoothDeviceState.connected));
-   await tester.pumpWidget(MaterialApp(
-       home: Scaffold(
-         body: VehicleControlsPageContent(mockDevice, mockFlutterBlue),
-       )));
-   await tester.pump();
-
-   expect(find.text(BluetoothDeviceState.connected.toString()), findsOneWidget);
-
- });
-
-//TODO STU FIX
-//  testWidgets("widget reflects state changes", (WidgetTester tester) async {
-//
-//    //ARRANGE
-//    var mockFlutterBlue = MockFlutterBlue();
-//    var mockDevice = MockBluetoothDevice();
-//    StreamController<BluetoothDeviceState> streamController = StreamController<BluetoothDeviceState>();
-//    when(mockDevice.state).thenAnswer((_) => Future.value(BluetoothDeviceState.connecting));
-//    when(mockDevice.onStateChanged()).thenAnswer((_) => streamController.stream);
-//    await tester.pumpWidget(MaterialApp(
-//        home: Scaffold(
-//          body: VehicleControlsPageContent(mockDevice, mockFlutterBlue),
-//        )));
-//
-//    //ACT
-//    await tester.pump();
-//
-//    //ASSERT
-//    expect(find.text(BluetoothDeviceState.connecting.toString()), findsOneWidget);
-//    expect(streamController.hasListener, true);
-//
-//    //ACT
-//    streamController.add(BluetoothDeviceState.connected);
-//    await tester.pumpAndSettle();
-//
-//    //ASSERT
-//    expect(find.text(BluetoothDeviceState.connected.toString()), findsOneWidget);
-//  });
 //
 //TODO STU FIX: This should be a unit test, not a widget test
 //  testWidgets("should run default command on startup", (WidgetTester tester) async {
